988289c9cc95a61548c1f94fd9855b12b58e5d20
Dzulfikar Dzulfikar <kardzulfi1805@gmail.com>
2023-05-14 14:37:39 +0700

    initial code

---
+++ MoodySnapper/MoodySnapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodySnapperApp.swift
+//  MoodySnapper
+//
+//  Created by Dzulfikar on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodySnapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
